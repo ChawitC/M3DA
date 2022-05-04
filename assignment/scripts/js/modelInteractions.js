5944f3edda153e849634962496a61eb761ad0a73
ChawitC <91472453+ChawitC@users.noreply.github.com>
2022-05-04 12:33:29 +0100

    04/05 - 2 Selectable render modes
    Now render modes are selectable between Polygon, Wireframe and Vertex mode without cycling
    implemented with simple logical programming in modelInteraction.js

--- assignment/scripts/js/modelInteractions.js
+++ assignment/scripts/js/modelInteractions.js
@@ -28,16 +28,79 @@ function animateModel() {
         document.getElementById('model2__RotationTimer2').setAttribute('enabled', 'false');
 }
 
+//simple counter logic that will allow manual switching between modes
+//Poly = 0
+//Wireframe = 1
+//Vertex = 2
+var currentmode = 0;
+
+function poly() {
+    var e = document.getElementById('model');
+    var f = document.getElementById('model2');
+    var g = document.getElementById('model3');
+
+    if (currentmode == 0) { }
+    else if (currentmode == 1) { //each toggles 4 times
+        for (var i = 0; i<4; i++){
+        e.runtime.togglePoints(true);
+        f.runtime.togglePoints(true);
+        g.runtime.togglePoints(true);
+        }
+    }
+    else if (currentmode == 2) { //each toggles 2 times
+        for (var i = 0; i<2; i++){
+        e.runtime.togglePoints(true);
+        f.runtime.togglePoints(true);
+        g.runtime.togglePoints(true);
+        }
+    }
+    currentmode = 0;
+}
+
 function wireframe() {
     var e = document.getElementById('model');
     var f = document.getElementById('model2');
     var g = document.getElementById('model3');
-    e.runtime.togglePoints(true);
-    e.runtime.togglePoints(true);
-    f.runtime.togglePoints(true);
-    f.runtime.togglePoints(true);
-    g.runtime.togglePoints(true);
-    g.runtime.togglePoints(true);
+
+    if (currentmode == 1) { }
+    else if (currentmode == 2) { //each toggles 4 times
+        for (var i = 0; i<4; i++){
+        e.runtime.togglePoints(true);
+        f.runtime.togglePoints(true);
+        g.runtime.togglePoints(true);
+        }
+    }
+    else if (currentmode == 0) { //each toggles 2 times
+        for (var i = 0; i<2; i++){
+        e.runtime.togglePoints(true);
+        f.runtime.togglePoints(true);
+        g.runtime.togglePoints(true);
+        }
+    }
+    currentmode = 1;
+}
+
+function vertex() {
+    var e = document.getElementById('model');
+    var f = document.getElementById('model2');
+    var g = document.getElementById('model3');
+
+    if (currentmode == 2) { }
+    else if (currentmode == 0) { //each toggles 4 times
+        for (var i = 0; i<4; i++){
+        e.runtime.togglePoints(true);
+        f.runtime.togglePoints(true);
+        g.runtime.togglePoints(true);
+        }
+    }
+    else if (currentmode == 1) { //each toggles 2 times
+        for (var i = 0; i<2; i++){
+        e.runtime.togglePoints(true);
+        f.runtime.togglePoints(true);
+        g.runtime.togglePoints(true);
+        }
+    }
+    currentmode = 2;
 }
 
 function resetview() {
@@ -53,6 +116,20 @@ var lightOn = true;
 function headlight() {
     lightOn = !lightOn;
     document.getElementById('model__headlight').setAttribute('headlight', lightOn.toString());
+    document.getElementById('model__headlight2').setAttribute('headlight', lightOn.toString());
+    document.getElementById('model__headlight3').setAttribute('headlight', lightOn.toString());
+}
+
+var OmniOn = true;
+
+function Omni() {
+    OmniOn = !OmniOn;
+    document.getElementById('model__Omni11').setAttribute('OmniOn', lightOn.toString());
+    document.getElementById('model__Omni12').setAttribute('OmniOn', lightOn.toString());
+    document.getElementById('model__Omni13').setAttribute('OmniOn', lightOn.toString());
+    document.getElementById('model__Omni14').setAttribute('OmniOn', lightOn.toString());
+    document.getElementById('model__Omni15').setAttribute('OmniOn', lightOn.toString());
+    document.getElementById('model__Omni16').setAttribute('OmniOn', lightOn.toString());
 }
 
 function cameraFront() {
